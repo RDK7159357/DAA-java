059ccd9cbbe75884ee82d9fcdfb3b386395359b3
RDK7159357 <155869647+RDK7159357@users.noreply.github.com>
2025-06-01 14:21:28 +0530

    7.1

--- section1/Prims.java
+++ section1/Prims.java
@@ -5,22 +5,22 @@ import java.util.*;
 public class Prims {
     static int prim(int[][] graph) {
         int n = graph.length, cost = 0;
-        boolean[] selected = new boolean[n];
-        int[] key = new int[n];
-        Arrays.fill(key, Integer.MAX_VALUE);
-        key[0] = 0;
+        boolean[] visited = new boolean[n];
+        int[] dist = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[0] = 0;
 
         for (int count = 0; count < n; count++) {
             int u = -1;
             for (int v = 0; v < n; v++)
-                if (!selected[v] && (u == -1 || key[v] < key[u])) u = v;
+                if ( !visited[v] && (u == -1 || dist[v] < dist[u])) u = v;
 
-            selected[u] = true;
-            cost += key[u];
+            visited[u] = true;
+            cost += dist[u];
 
             for (int v = 0; v < n; v++)
-                if (graph[u][v] != 0 && !selected[v] && graph[u][v] < key[v])
-                    key[v] = graph[u][v];
+                if (graph[u][v] != 0 &&  !visited[v] && graph[u][v] < dist[v])
+                    dist[v] = graph[u][v];
         }
         return cost;
     }
